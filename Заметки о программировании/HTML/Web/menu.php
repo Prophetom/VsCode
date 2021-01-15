dfc2c486cbf3457d4e6869e6f97078235ccf1633
proph3tum <nikalekkseevvv@yandex.ru>
2021-01-15 22:34:39 +0700

    playing with folders

--- Заметки о программировании/HTML/Web/menu.php
+++
@@ -1,8 +0,0 @@
-<div class="content">
-			<div class="header">
-				<a href="index.php">Главная</a>
-				<a href="puzzle.php">Загадки</a>
-				<a href="guess.php">Угадайка</a>
-				<a href="guessmp.php">Угадайка для двух игроков</a>
-				<a href="password.php">Генератор паролей</a>
-</div>
